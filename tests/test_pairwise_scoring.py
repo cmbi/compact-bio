52f3c7cc20722842819f073bc4b42930ddcdc732
joerivstrien <joerivanstrien@gmail.com>
2022-09-24 15:14:40 +0200

    created test setuop

---
+++ tests/test_pairwise_scoring.py
@@ -0,0 +1,13 @@
+from rbomcl import pairwise_scoring as ps
+
+
+def test_rename_indices():
+    left = ['a','b','c']
+    right = ['b','c','d']
+    mapping = {'a':'b'}
+    expected = (
+        ['a|b','b','c'],
+        ['a|b','c','d']
+        )
+    assert ps.rename_indices(
+        left,right,mapping) == expected 
