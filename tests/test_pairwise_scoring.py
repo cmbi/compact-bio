76a0b5429ac3d82aedbcec71fb478785aaf0c8a7
Joeri <joerivanstrien@gmail.com>
2023-01-11 17:55:58 +0100

    started with fixtures

--- tests/test_pairwise_scoring.py
+++ tests/test_pairwise_scoring.py
@@ -1,16 +1,23 @@
 from compact import pairwise_scoring as ps
+import pytest
+
+@pytest.fixture
+def data():
+    return {
+        'left':['a','b','c'],
+        'right': ['b','c','d'],
+        'mapping':{'a':'b'},
+    }
 
 
-def test_rename_indices():
-    left = ['a','b','c']
-    right = ['b','c','d']
-    mapping = {'a':'b'}
+
+def test_rename_indices(data):
     expected = (
         ['a|b','b','c'],
         ['a|b','c','d']
         )
     assert ps.rename_indices(
-        left,right,mapping) == expected
+        data['left'],data['right'],data['mapping']) == expected
     
 def assign_matched():
     pass
