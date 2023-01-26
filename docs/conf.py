e0e8cba1c9cf312e2defec587fb7b9737c29c208
Joeri van Strien <joerivanstrien@gmail.com>
2023-01-26 15:23:33 +0100

    added mocked dependencies to docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,19 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = [
+    'numpy',
+    'pandas',
+    'rbo']
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 sys.path.insert(0, os.path.abspath(os.path.join('..','src')))
 
 
